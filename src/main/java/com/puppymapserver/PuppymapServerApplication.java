50aa9cf1a3be01e8f832195d142c4a952e9f1282
SangkiHan <tkdrl8908@naver.com>
2026-04-21 16:12:50 +0900

    feat: first commit

---
+++ src/main/java/com/puppymapserver/PuppymapServerApplication.java
@@ -0,0 +1,13 @@
+package com.puppymapserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuppymapServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PuppymapServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/puppymapserver/PuppymapServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puppymapserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuppymapServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
