398790af39b270289b0f796a1388589d9eb36625
SangkiHan <tkdrl8908@naver.com>
2026-04-21 18:28:00 +0900

    test(docs): StorageControllerDocsTest 추가

---
+++ src/test/java/docs/storage/StorageControllerDocsTest.java
@@ -0,0 +1,64 @@
+package docs.storage;
+
+import com.puppymapserver.storage.controller.StorageController;
+import com.puppymapserver.storage.enums.BucketKind;
+import com.puppymapserver.storage.service.S3StorageService;
+import docs.RestDocsSupport;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.restdocs.payload.JsonFieldType;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.multipart;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class StorageControllerDocsTest extends RestDocsSupport {
+
+    private final S3StorageService s3StorageService = mock(S3StorageService.class);
+
+    @Override
+    protected Object initController() {
+        return new StorageController(s3StorageService);
+    }
+
+    @DisplayName("파일 업로드 API")
+    @Test
+    void 파일_업로드() throws Exception {
+        given(s3StorageService.upload(any(), any()))
+                .willReturn("https://s3.example.com/puppy-walk/image.jpg");
+
+        MockMultipartFile file = new MockMultipartFile(
+                "file", "image.jpg", MediaType.IMAGE_JPEG_VALUE, "image-data".getBytes()
+        );
+
+        mockMvc.perform(multipart("/api/v1/storage/{bucketKind}", BucketKind.WALK_PHOTO)
+                        .file(file))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("storage-upload",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(
+                                parameterWithName("bucketKind").description("버킷 유형 (PUPPY_PROFILE, WALK_PHOTO, PET_ITEM_PHOTO, USER_PROFILE, COMMUNITY_POST)")
+                        ),
+                        requestParts(
+                                partWithName("file").description("업로드할 파일")
+                        ),
+                        responseFields(
+                                fieldWithPath("statusCode").type(JsonFieldType.NUMBER).description("HTTP 상태 코드"),
+                                fieldWithPath("httpStatus").type(JsonFieldType.STRING).description("HTTP 상태"),
+                                fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메시지"),
+                                fieldWithPath("data").type(JsonFieldType.STRING).description("업로드된 파일 URL")
+                        )
+                ));
+    }
+}
