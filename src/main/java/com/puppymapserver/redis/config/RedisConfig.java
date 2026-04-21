20572732561ac9d64d72099667e5f7e6a3d70b2a
SangkiHan <tkdrl8908@naver.com>
2026-04-21 18:18:03 +0900

    fix: RedisConfig @Profile("!test") 추가 - 테스트 시 Redis 연결 시도 방지

--- src/main/java/com/puppymapserver/redis/config/RedisConfig.java
+++ src/main/java/com/puppymapserver/redis/config/RedisConfig.java
@@ -5,6 +5,7 @@ import io.lettuce.core.SslOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
@@ -12,6 +13,7 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Configuration
+@Profile("!test")
 public class RedisConfig {
 
     @Value("${spring.data.redis.host}")
